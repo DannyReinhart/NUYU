e5842dcea4698386ab5c7dcfd9ddcd5384160419
DannyReinhart <Danreinrm@gmail.com>
2014-08-09 16:49:17 -0700

    initial commit with single view

---
+++ NUYU/NUYU/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NUYU
+//
+//  Created by Daniel Reinhart on 8/9/14.
+//  Copyright (c) 2014 Daniel Reinhart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NUYU/NUYU/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  NUYU
+//
+//  Created by Daniel Reinhart on 8/9/14.
+//  Copyright (c) 2014 Daniel Reinhart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
